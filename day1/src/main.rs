05ed6b9914eefb0437052c72822e40ba5f182a71
Raz <raz@raztech.io>
2023-12-01 18:39:11 +0100

    Complete day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,5 +1,9 @@
 use std::{io::BufRead, time::Instant};
 
+const NUMS: &[&[u8]] = &[
+    b"one", b"two", b"three", b"four", b"five", b"six", b"seven", b"eight", b"nine",
+];
+
 fn input() -> Vec<String> {
     std::io::stdin()
         .lock()
@@ -12,7 +16,7 @@ fn main() {
     let time = Instant::now();
     let input = input();
     part1(&input);
-    // part2(&input);
+    part2(&input);
     println!("Time elapsed is {:?}", time.elapsed())
 }
 
@@ -27,10 +31,32 @@ fn part1(input: &[String]) {
             ((first_digit - b'0') * 10 + (last_digit - b'0')) as i32
         })
         .sum();
-
     println!("Part 1 answer: {:?}", result);
 }
 
-// fn part2(input: &Vec<usize>) {
-//     println!("Part 2 answer: {:?}", result);
-// }
+fn part2(input: &[String]) {
+    let result: usize = input
+        .iter()
+        .map(|line| {
+            let bytes = line.as_bytes();
+            let first_digit = (0..bytes.len()).find_map(|i| find_num(bytes, i)).unwrap();
+            let last_digit = (0..bytes.len())
+                .rev()
+                .find_map(|i| find_num(bytes, i))
+                .unwrap();
+            first_digit * 10 + last_digit
+        })
+        .sum();
+    println!("Part 2 answer: {:?}", result);
+}
+
+fn find_num(line: &[u8], i: usize) -> Option<usize> {
+    line[i]
+        .is_ascii_digit()
+        .then_some((line[i] - b'0') as usize)
+        .or(NUMS
+            .iter()
+            .enumerate()
+            .find(|(_, name)| line[i..].starts_with(name))
+            .map(|(num, _)| num + 1))
+}
