7a321373b7d417d9336112f56870133a0bc2ee4b
Raz <raz@raztech.io>
2023-12-15 00:22:51 +0100

    Complete day 10 part 2

--- day10/src/main.rs
+++ day10/src/main.rs
@@ -63,6 +63,89 @@ fn find_loop_length(
     }
 }
 
+/// Calculate the length at each step for the first loop completion
+fn calculate_lengths(
+    lines: &[String],
+    start: (usize, usize),
+    map: &HashMap<(Direction, char), Direction>,
+) -> HashMap<(usize, usize), usize> {
+    let base = [
+        Direction::Up,
+        Direction::Down,
+        Direction::Left,
+        Direction::Right,
+    ];
+
+    let mut length_store = HashMap::new();
+
+    'base: for dir in base {
+        let mut point = start;
+        let mut len = 0;
+        let mut dirx = dir;
+
+        loop {
+            len += 1;
+            point = next_point(point, dirx);
+            let y = point.0;
+            let x = point.1;
+
+            if let Some(line) = lines.get(y) {
+                if let Some(&new_direction) = map.get(&(dirx, line.chars().nth(x).unwrap())) {
+                    dirx = new_direction;
+                    length_store.insert(point, len);
+                }
+                if line.chars().nth(x).unwrap_or('\0') == 'S' {
+                    length_store.insert(point, len);
+                    break 'base;
+                }
+            } else {
+                break;
+            }
+        }
+    }
+
+    length_store
+}
+
+fn count_points_inside(
+    lines: &[String],
+    lengths: &HashMap<(usize, usize), usize>,
+    start: (usize, usize),
+) -> usize {
+    let mut total_count = 0;
+    for (row, line) in lines.iter().enumerate() {
+        let mut scan = false;
+        let mut count = 0;
+
+        for (col, _) in line.chars().enumerate() {
+            // If scan is true and current tile is not part of the loop, this must mean this tile
+            // is contained within the loop grid
+            if scan && lengths.get(&(row, col)).is_none() {
+                count += 1;
+            }
+
+            // Check if the current point and the point below it are on the loop path
+            if let (Some(&a), Some(&b)) = (lengths.get(&(row, col)), lengths.get(&(row + 1, col))) {
+                // Calculate relative position on the loop using modulo arithmetic to handle the
+                // cyclic nature
+                let loop_crossing_check: isize = (((a as isize - b as isize)
+                    % lengths[&start] as isize)
+                    + lengths[&start] as isize)
+                    % lengths[&start] as isize;
+                // If loop_crossing_check is 1 or loop length - 1, the loop boundary has been
+                // crossed
+                if [1, lengths[&start] - 1].contains(&(loop_crossing_check as usize)) {
+                    scan = !scan;
+                }
+            }
+        }
+
+        total_count += count;
+    }
+
+    total_count
+}
+
 fn input() -> Vec<String> {
     std::io::stdin()
         .lock()
@@ -80,8 +163,8 @@ fn solve(input: &[String]) -> usize {
     let map = build_direction_map();
 
     if let Some(start) = start {
-        // We check all four directions (BFS), we know that only only starting directions will complete
-        // the loop
+        // We check all four directions (DFS), we know that only starting two directions will complete
+        // the same loop
         let base = [
             Direction::Up,
             Direction::Down,
@@ -104,9 +187,28 @@ fn main() {
     let time = Instant::now();
     let input = input();
     part1(solve(&input));
+    part2(&input);
     println!("Time elapsed is {:?}", time.elapsed())
 }
 
 fn part1(result: usize) {
     println!("Part 1 answer: {:?}", result);
+}
+
+fn part2(input: &[String]) {
+    // Find the starting position in the grid
+    let start = input
+        .iter()
+        .enumerate()
+        .find_map(|(i, line)| line.chars().position(|c| c == 'S').map(|j| (i, j)));
+    let map = build_direction_map();
+
+    let result = if let Some(start) = start {
+        let lengths = calculate_lengths(input, start, &map);
+        count_points_inside(input, &lengths, start)
+    } else {
+        panic!("Start not found")
+    };
+
+    println!("Part 2 answer: {:?}", result);
 }
