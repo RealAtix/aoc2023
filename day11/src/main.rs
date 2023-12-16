5c82e25d7205ee53c36350fcb38270b89c0e4955
Raz <raz@raztech.io>
2023-12-17 00:43:17 +0100

    Complete day 11 part 2 + smarter expansion

--- day11/src/main.rs
+++ day11/src/main.rs
@@ -1,93 +1,99 @@
-use std::{
-    collections::{HashMap, HashSet},
-    io::BufRead,
-    time::Instant,
-};
+use std::{collections::HashSet, io::BufRead, time::Instant};
 
-fn input() -> HashMap<(usize, usize), char> {
-    let mut expand_rows = HashSet::new();
-    let mut expand_cols = HashSet::new();
-    // let mut universe = HashMap::new();
+type GalaxyPairs = Vec<((usize, usize), (usize, usize))>;
 
+fn input() -> (GalaxyPairs, HashSet<usize>, HashSet<usize>) {
     let input: Vec<String> = std::io::stdin()
         .lock()
         .lines()
         .map(|line| line.unwrap())
         .collect();
 
-    // Track universe expansion
+    // Track universe expansion and galaxies
+    let mut expand_rows = HashSet::new();
+    let mut expand_cols = HashSet::new();
+    let mut galaxies: Vec<(usize, usize)> = Vec::new();
     for (y, row) in input.iter().enumerate() {
         if !row.contains('#') {
             expand_rows.insert(y);
         }
         for (x, ch) in row.chars().enumerate() {
-            // universe.insert((y, x), ch);
             if ch == '.' && !input.iter().any(|row| row.chars().nth(x) == Some('#')) {
                 expand_cols.insert(x);
+            } else if ch == '#' {
+                galaxies.push((y, x));
             }
         }
     }
 
-    // Return expanded universe
-    let mut universe = HashMap::new();
-    let col_count = input[0].len() + expand_cols.len(); // Assuming all rows are the same length
-    for (y, row) in input.iter().enumerate() {
-        let expanded_y = y + expand_rows.iter().filter(|&&ry| ry < y).count();
-        for (x, ch) in row.chars().enumerate() {
-            let expanded_x = x + expand_cols.iter().filter(|&&cx| cx < x).count();
-            universe.insert((expanded_y, expanded_x), ch);
-            if expand_cols.contains(&x) {
-                universe.insert((expanded_y, expanded_x + 1), ch);
-            }
-        }
-        if expand_rows.contains(&y) {
-            for x in 0..col_count {
-                universe.insert((expanded_y + 1, x), '.');
-            }
-        }
-    }
+    let pairs: GalaxyPairs = galaxies
+        .iter()
+        .enumerate()
+        .flat_map(|(i, loc1)| galaxies.iter().skip(i + 1).map(|loc2| (*loc1, *loc2)))
+        .collect();
 
-    universe
+    (pairs, expand_rows, expand_cols)
+}
+
+// fn get_expanded_point(
+//     original: usize,
+//     expansions: &HashSet<usize>,
+//     expansion_factor: usize,
+// ) -> usize {
+//     original + (expansions.iter().filter(|&&e| e < original).count() * expansion_factor)
+// }
+
+fn get_shortest_galaxy_paths(
+    galaxy_pairs: &GalaxyPairs,
+    expand_rows: &HashSet<usize>,
+    expand_cols: &HashSet<usize>,
+    expansion_factor: usize,
+) -> isize {
+    // Factor in the expansion of the universe
+    let get_expanded_point =
+        |original: usize, expansions: &HashSet<usize>, expansion_factor: usize| -> usize {
+            original + (expansions.iter().filter(|&&e| e < original).count() * expansion_factor)
+        };
+
+    // Sum the shortest path
+    // https://en.wikipedia.org/wiki/Taxicab_geometry
+    galaxy_pairs
+        .iter()
+        .map(|(loc1, loc2)| {
+            let loc1_expanded = (
+                get_expanded_point(loc1.0, expand_rows, expansion_factor),
+                get_expanded_point(loc1.1, expand_cols, expansion_factor),
+            );
+            let loc2_expanded = (
+                get_expanded_point(loc2.0, expand_rows, expansion_factor),
+                get_expanded_point(loc2.1, expand_cols, expansion_factor),
+            );
+            (loc1_expanded.1 as isize - loc2_expanded.1 as isize).abs()
+                + (loc1_expanded.0 as isize - loc2_expanded.0 as isize).abs()
+        })
+        .sum()
 }
 
 fn main() {
     let time = Instant::now();
-    let universe = input();
-    part1(&universe);
-    // part2(&input);
+    let (galaxy_pairs, expand_rows, expand_cols) = input();
+    part1(&galaxy_pairs, &expand_rows, &expand_cols);
+    part2(&galaxy_pairs, &expand_rows, &expand_cols);
     println!("Time elapsed is {:?}", time.elapsed())
 }
 
-fn part1(universe: &HashMap<(usize, usize), char>) {
-    // Get pairs
-    let galaxies: Vec<(usize, usize)> = universe
-        .iter()
-        .filter(|(_, &kind)| kind == '#')
-        .map(|(&coord, _)| coord)
-        .collect();
-    let pairs: Vec<((usize, usize), (usize, usize))> = galaxies
-        .iter()
-        .enumerate()
-        .flat_map(|(i, loc1)| {
-            galaxies
-                .iter()
-                .skip(i + 1)
-                .map(|loc2| (loc1.clone(), loc2.clone()))
-        })
-        .collect();
-
-    // Sum the shortest path
-    // https://en.wikipedia.org/wiki/Taxicab_geometry
-    let result: isize = pairs
-        .iter()
-        .map(|(loc1, loc2)| {
-            (loc1.1 as isize - loc2.1 as isize).abs() + (loc1.0 as isize - loc2.0 as isize).abs()
-        })
-        .sum();
-
-    println!("Part 1 answer: {:?}", result);
+fn part1(galaxy_pairs: &GalaxyPairs, expand_rows: &HashSet<usize>, expand_cols: &HashSet<usize>) {
+    const EXPANSION_FACTOR: usize = 1;
+    println!(
+        "Part 1 answer: {:?}",
+        get_shortest_galaxy_paths(galaxy_pairs, expand_rows, expand_cols, EXPANSION_FACTOR)
+    );
 }
 
-// fn part2(input: &[String]) {
-//     println!("Part 2 answer: {:?}", result);
-// }
+fn part2(galaxy_pairs: &GalaxyPairs, expand_rows: &HashSet<usize>, expand_cols: &HashSet<usize>) {
+    const EXPANSION_FACTOR: usize = 999_999;
+    println!(
+        "Part 2 answer: {:?}",
+        get_shortest_galaxy_paths(galaxy_pairs, expand_rows, expand_cols, EXPANSION_FACTOR)
+    );
+}
